6cf55db850f4c7892758c31b32d438766afa6046
Bonambonamraj@gmail.com <akhilchinnamsetti@gmail.com>
2025-07-22 15:50:15 +0530

    Deploy updated vite.config.js and build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,24 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/hariharanursery/', // 🔥 Your repo name
+  build: {
+    outDir: 'docs', // 🔥 Output folder GitHub Pages uses
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
